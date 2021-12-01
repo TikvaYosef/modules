3b6f043fb9e068a3aa8592cb87c15903b4a0ef49
TikvaYosef <tikvayosef13@gmail.com>
2021-12-01 09:39:15 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,2 @@
+import name from "./moduls.js"
+
---
+++ moduls.js
@@ -0,0 +1,12 @@
+const name  = "Tikva";
+
+
+
+console.log(name)
+
+function PrintName(){
+    console.log("Hello")
+}
+PrintName()
+
+export default name;
